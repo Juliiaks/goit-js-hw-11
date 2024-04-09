086377bd69d0de3d85545c16bb98a69a695d99f0
Juliiaks <juliakharetonchuk@gmail.com>
2024-04-09 03:03:31 +0300

    almost done

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,21 +1,30 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
+import { addPictures } from "./render-functions";
 
-import { searchWord } from "../main";
+
+
 
 export function apiPixabay(searchWord) {
 const API_KEY = "43264950-7d4cd6be2017577f15438949e";
 const params = new URLSearchParams({
-    keys: API_KEY,
+    key: API_KEY,
     q: searchWord,
     image_type: "photo",
     orientation: "horizontal",
     safesearch: true
 });
 
-    fetch(`https://pixabay.com/api?${params}`)
+    fetch(`https://pixabay.com/api/?${params}`)
         .then(response => {
             if (!response.ok) {
+                throw new Error(response.status);
+                
+            }
+            return response.json()
+        })
+        .then(data => {
+            if (data.hits.length === 0) {
                 iziToast.show({
                     message: `Sorry, there are no images matching your search query. Please try again!`,
                     messageColor: "#fff",
@@ -24,11 +33,15 @@ const params = new URLSearchParams({
                     progressBar: false,
                     close: false,
                     timeout: 5000,
-                });
+            });
+           
             }
-            return response.json()
+            console.log(data);
+            addPictures(data.hits);
         })
-        .then(data => { console.log(data) })
-        .catch(error => { console.log("catch", error); });
+        .catch(error => {
+            console.log("catch", error);
+             });
    
 }
+
--- src/main.js
+++ src/main.js
@@ -1,8 +1,51 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css"
 import { apiPixabay } from "./js/pixabay-api";
+// import SimpleLightbox from "simplelightbox";
+// import "simplelightbox/dist/simple-lightbox.min.css";
 
-const input = document.querySelector("#pictures");
-export const searchWord = input.value;
 
 
+
+
+const form = document.querySelector(".form");
+const searchButton = document.querySelector("button");
+
+form.addEventListener("submit", handleSubmit);
+
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+    const input = document.querySelector("#pictures");
+    const searchWord = input.value.trim();
+    
+    if (searchWord === "") {
+        console.log("empty");
+          iziToast.show({
+                    message: `Sorry, there are no images matching your search query. Please try again!`,
+                    messageColor: "#fff",
+                    position: "topRight",
+                    backgroundColor: "#ef4040",
+                    progressBar: false,
+                    close: false,
+                    timeout: 5000,
+            });
+       return;
+    };
+    // showLoader();
+    apiPixabay(searchWord);
+    // hideLoader();
+    form.reset()  
+}
+
+// function showLoader() {
+//     const loader = document.querySelector(".loader").classList.add("active");
+//     return loader;
+        
+//     }
+
+// function hideLoader() {
+//     const loader = document.querySelector(".loader").classList.remove("active")
+//     return loader;
+// }
